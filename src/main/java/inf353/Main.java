0a5461fb24af491edb8b59fb8cbe7b8018133482
Yacineos <96418831+Yacineos@users.noreply.github.com>
2022-10-09 13:27:10 +0200

    new main qui fonctionne

--- src/main/java/inf353/Main.java
+++ src/main/java/inf353/Main.java
@@ -3,7 +3,19 @@ package inf353;
 public class Main{
     public static void main(String[] args) {
         DictionnaireNaif dictionnaire = new DictionnaireNaif(3);
-           
+        dictionnaire.d[0] = 'T';
+        dictionnaire.d[1] = 'e';
+        dictionnaire.d[2] = 's';
+        dictionnaire.d[3] = 0;
+        dictionnaire.d[4] = 't';
+        
+        dictionnaire.d[7] = 0;
+        dictionnaire.d[8] = ' ';
+        dictionnaire.d[9] = 0;
+        //dictionnaire.d[10] = 'g';
+        //dictionnaire.d[11] = 0;
+
+        System.out.println(dictionnaire.indiceMot("es"));    
 
     }
 }

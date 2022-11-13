be351285f89d8abf3fbe5464e4a55dfd30f14d7f
Angeline Ali <angeline.ali@etu.univ-grenoble-alpes.fr>
2022-11-13 12:29:45 +0100

    j'ai utilisé la méthode read() à la place de readLine() dans la classe LecteurDocumentNaif

--- src/main/java/inf353/LecteurDocumentNaif.java
+++ src/main/java/inf353/LecteurDocumentNaif.java
@@ -1,40 +1,59 @@
 package inf353;
-import java.io.File ;
-import java.io.FileReader;
-import java.io.BufferedReader;
-
+import java.io.*;
 /**
  * Classe LecteurDocumentNaif qui implémente un accès séquenciel de modèle 1 d'élemnts de type String.
  * Elle donne accès à un texte contenu dans un fichier, mot par mot.
  */
 public class LecteurDocumentNaif implements AccesSequentielModele1<String> {
 
-    public String texte = "";
     public String eCourant;
     public boolean fds = false;
-    public int i = 0;
+    public int premier;
+    public int i ;
+    public BufferedReader br;
+    public String path;
+    
+    public LecteurDocumentNaif(String path) throws java.io.IOException {
+        this.path = path;
+        System.setProperty( "file.encoding", "UTF-8" );
 
-   public LecteurDocumentNaif(String path) throws java.io.IOException {
-    File f = new File(path);
-    BufferedReader br = new BufferedReader(new FileReader(f));
+        //Lecture du fichier de chemin path
+        br = new BufferedReader(new FileReader(new File(path)));
 
-    String ligne;
+        //Stock du tout premier caractère dans l'int premier
+        premier = br.read();
 
-    //on stock tout les mots dans texte
-    while ((ligne = br.readLine())!=null){
-        texte += ligne + " ";
+        //Si le fichier n'est pas vide
+        if(premier != -1){ 
+            br.mark(premier);
+        }
     }
-
-    System.out.println(texte);
-   }
    
    
        /**
      * Initialisation du parcours.
      */
-    public void demarrer() {
-        while( i != texte.length()  && !charAccepte(texte.charAt(i))){
-            i++;
+    public void demarrer() throws java.io.IOException{
+
+        //si fin de séquence, il faut relire le fichier
+        if(finDeSequence()){
+            fds = false;
+            System.setProperty( "file.encoding", "UTF-8" );
+            br = new BufferedReader(new FileReader(new File(path)));
+            premier = br.read();
+            if(premier != -1){ 
+                br.mark(premier);
+            }
+        }
+
+        //Si le fichier n'est pas vide
+        if(premier != -1){ 
+            br.reset();
+        }
+
+        i = premier;
+        while((i != -1 ) && !charAccepte(i)){
+            i = br.read();
         }
         avancer();
     }
@@ -42,18 +61,23 @@ public class LecteurDocumentNaif implements AccesSequentielModele1<String> {
     /**
      * Passage à l'élément suivant
      */ 
-    public void avancer() {
-        eCourant = "";
-        if (i == texte.length() ){
+    public void avancer() throws java.io.IOException {
+        
+        //si on est la fin du fichier
+        if (i == -1){
             fds = true;
+            br.close();
         }
-        while(i != texte.length() && charAccepte(texte.charAt(i))){
-            eCourant = eCourant + texte.charAt(i);
-            i++;
+
+        eCourant = "" ;
+        while((i != -1 ) && charAccepte(i)){
+            eCourant = eCourant + ((char)i);
+            i =br.read();
         }
-        while(i != texte.length() && !charAccepte(texte.charAt(i))){
-            i++;
+        while((i != -1 ) && !charAccepte(i)){
+            i =br.read();
         }
+
     }
 
     /**
@@ -72,7 +96,16 @@ public class LecteurDocumentNaif implements AccesSequentielModele1<String> {
         return eCourant;
     }
 
-    public static boolean charAccepte(char c) {
-        return ( c>47 && c<58)||( c>64 && c<91 ) || ( c>96 && c<123 ) || (c>191 && c<256);
+    public static boolean charAccepte(int c) {
+        return ( c>47 && c<58) || ( c>64 && c<91 ) || ( c>96 && c<123 ) || (c>191 && c<256);
+    }
+
+    public void afficher() throws java.io.IOException {
+        LecteurDocumentNaif l = new LecteurDocumentNaif(path);
+        l.demarrer();
+        while(!l.finDeSequence()){
+            System.out.println(l.elementCourant());
+            l.avancer();
+        }
     }
 }
